d43f9fc00709c98e83ffc51f701c2eadef4cf92a
Gopal Kadam <gopalkadam111@gmail.com>
2024-08-29 21:26:17 +0530

    first

---
+++ loanapp.py
@@ -0,0 +1,45 @@
+from flask import Flask, request
+import pickle
+import sklearn
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello_world():
+    return "<p>This is WebAPI learning!</p>"
+
+@app.route("/ping", methods=['GET'])
+def pinger():
+    return "<p>Hello i am Gopal Kadam</p>"
+
+
+model_pickle = open("./classifier.pkl", "rb")
+clf = pickle.load(model_pickle)
+
+@app.route("/predict", methods=['POST'])
+def prediction():
+    loan_req = request.get_json()
+
+    if loan_req['Gender'] == "Male":
+        Gender = 0
+    else:
+        Gender = 1
+
+    if loan_req['Married'] == "Yes":
+        Married = 1
+    else:
+        Married = 0
+
+    ApplicantIncome = loan_req['ApplicantIncome']
+    LoanAmount = loan_req['LoanAmount']
+    Credit_History = loan_req['Credit_History']
+
+    result = clf.predict([[Gender, Married, ApplicantIncome, LoanAmount, Credit_History]])
+
+    if result == 0:
+        pred = "Rejected"
+    else:
+        pred = "Approved"
+
+    return {"loan_approval_status": pred}
+
